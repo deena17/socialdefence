bdb7024321713dbc6bb3312bc48291a964397784
deena_projects <deenadayalan17@gmail.com>
2017-12-05 12:28:25 +0530

    Add files via upload

---
+++ old-index.php
@@ -0,0 +1,299 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<?php session_start(); ?>
+<head>
+<title>Department of Social Defence - Acts & Rules</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<script type="text/javascript" src="style/js/jquery-1.12.3.min.js"></script>
+<script type="text/javascript" src="style/js/bootstrap.min.js"></script>
+<link rel="stylesheet" type="text/css" href="style/css/bootstrap.min.css">
+<script src="style/js/font.js"></script>
+<link rel="stylesheet" type="text/css" href="style/css/w3.css">
+<link rel="stylesheet" type="text/css" href="style/css/menu.css">
+<link rel="stylesheet" type="text/css" href="style/css/social-defence.css">
+<link rel="stylesheet" type="text/css" href="style/css/font-change.css">
+<style>
+marquee li{list-style-type:none;margin:10px 0 0 5px;border-bottom:1px solid #48C9B0;border-left:15px solid #48C9B0;background-color:#A2D9CE;}
+marquee li:hover{border-left:15px solid #48C9B0;background-color:#A2D9CE;padding-left:3px} marquee li,a:hover{text-decoration:none}
+.links img{border:2px solid maroon !important;border-radius:5px;margin-bottom:0px;margin-top:2px}
+</style>
+</head>
+<body>
+<script language="JavaScript">
+    function function1(){
+        document.all.myMarquee.stop();
+    }
+    function function2(){
+        document.all.myMarquee.start();
+    }
+</script>
+<div class="wrapper">
+<div style="background-image:url(images/background.png)">
+<div class="row">
+<div class="col-lg-8 col-md-8 col-sm-8">
+<img src="images/logo/logo.png"/></div>
+<div class="col-lg-4 col-md-4 col-sm-4">
+<div id="fontlinks">
+		<a href="#" id="incfont" class="button buttonfont">A+</a>
+		<a href="#" id="decfont" class="button buttonfont">A-</a>
+                <a href="#" id="black" class="buttonblack buttonfont">A</a>
+                <a href="#" id="white" class="buttonwhite buttonfont">A</a>
+             		</div>
+</div></div>
+<div class="menu">
+<ul class="main-navigation">
+  <li><a class="w3-hover-blue" href="index.php"><img src="images/home.png" width="35" height="35" style="margin-top:-8px"></a></li>
+  <li><a class="w3-hover-blue" href="#">Organization</a>
+  <ul>
+      <li><a href="org-struct.html">Organizational Structure</a></li>
+      <li><a href="history.html">History</a></li>
+      <li><a href="admin-setup.html">Administrative Setup</a></li>
+  </ul>
+  </li>
+  <li><a class="w3-hover-blue" href="citizen-charters.html">Citizen Charter</a></li>
+  <li><a class="w3-hover-blue" href="#">Schemes & Services
+        <span class="caret"></span></a>
+     <ul>
+        <li><a href="ujjawala.html">Ujjawala Scheme</a></li>
+          <li><a href="swadhar.html">Swadhar Scheme</a></li>
+	  <li><a href="child-line.html">Child Line</a></li>
+          <li><a href="icps.html">ICPS</a></li>
+	  <li><a href="de-addiction-program.html">De-addiction Program (IRCA)</a></li>
+        </ul>
+      </li>
+   <li><a class="w3-hover-blue" href="#">Documents
+        <span class="caret"></span></a>
+        <ul>
+        <li> <a href="acts-rules.html">Acts and Rules</a></li>
+          <li><a href="govt-orders.html">G.O's </a></li>
+          <li><a href="policies.html">Policies</a></li>
+          <li><a href="download.html">Downloads</a></li>
+	  <li><a href="notifications.html">Notifications</a></li>
+        </ul>
+      </li>
+    <li><a class="w3-hover-blue" href="#">RTI
+        <span class="caret"></span></a>
+        <ul>
+         <li> <a href="docs/acts/RTI.pdf"target="_blank">RTI Act</a></li>
+         <li> <a href="http://www.tnsic.gov.in/"target="_blank">Tamil Nadu Information Commission</a></li>
+        </ul>
+      </li>
+   <li>
+        <a class="w3-hover-blue" href="#">Institutions
+        <span class="caret"></span></a>
+        <ul>
+         <li>  <a href="juvenile-justice.html">Juvenile Justice Boards</a></li>
+          <li> <a href="child-welfare.html">Child Welfare Committees</a></li>
+         <li> <a href="#">Government run Homes<span class="caret"></span></a>
+           <ul>
+           <li><a href="govt-homes.html">Children Homes</a></li>
+           <li><a href="govt-obshomes.html">Observation Homes</a></li>
+           <li><a href="govt-aftercare.html">AfterCare Institutions</a> </li>
+           <li><a href="govt-special.html">Special Homes</a></li>
+           <li><a href="govt-receptionunits.html">Reception Units</a></li></ul>
+           <li><a href="#">NGO run Homes <span class="caret"></span></a>
+           <ul>
+           <li><a  href="non-childhomes.html">Children Homes</a></li>
+          <li> <a  href="non-obshomes.html">Observation Homes</a></li>
+          <li> <a  href="resource-centers.html">Resource Centers</a></li>
+          <li> <a  href="ngo-receptionunits.html">Reception Units</a></li>
+	   <li><a  href="de-addiction.html">De-Addiction Centers</a></li>
+           <li><a  href="ujjawala.html">Ujjawala</a></li>
+           <li><a  href="swadhar.html">Swadhar</a></li></ul>
+           <li><a href="#">Registration Details Of CCIs <span class="caret"></span></a>
+          <ul>
+          <li><a  href="registered.html">Registered CCIs</a></li>
+          <li><a  href="applied-renewal.html">CCIs applied for renewal</a></li>
+         <li> <a  href="notapplied-renewal.html">CCIs not applied for renewal</a> </li>
+	  <li><a  href="applied-regis.html">Unregistered CCIs applied for Registration</a></li>
+          <li><a  href="notapplied-regis.html">Unregistered CCIs not applied for Registration</a></li>
+        </ul></ul>
+      </li>
+  <li><a class="w3-hover-blue" href="#">Publications
+        <span class="caret"></span></a>
+        <ul>
+         <li><a href="annual-reports.html">Annual Reports</a></li>
+         <li><a href="policy-note.html">Policy Note</a></li>
+         <li><a href="iec.html">IEC</a></li>
+        </ul>
+      </li>
+ <li><a class="w3-hover-blue" href="#">Contacts
+        <span class="caret"></span></a>
+        <ul>
+        <li><a href="contact-dept.html">Directorate of Social Defence</a></li>
+        <li><a href="contact-reg.html">Regional Office</a></li>
+        <li><a href="contact-scps.html">State Child Protection Society</a></li> 
+        <li><a href="docs/Contact/DCPU.pdf" target="_blank">District Child Protection Units </a></li>
+                <li><a href="contacts.html">State Legal Services Authority</a></ul></li>
+<li><a class="w3-hover-blue" href="gallery.html">Gallery</a></li>
+</ul>
+</div></div>
+<marquee direction="left" scrollamount="5"onmouseover="this.stop()" onmouseout="this.start()" class="w3-text-purple"><strong>Child Help Line Call : 1098 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Women Help Line Call :1091 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<!--இத்தளத்தைத் தமிழில் பார்க்க &nbsp;&nbsp;</strong><font color="blue"><a href="tamil/index.php">இங்கே சுட்டவும்</a></font--></marquee>
+<br>
+<div class="content">
+<div class="row">
+<div class="col-md-7 main" style="margin-left:30px">
+<div id="myCarousel" class="carousel slide box" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="images/slide/sd1.jpg" alt="Chania" height="300">
+    </div>
+
+    <div class="item">
+      <img src="images/slide/sd2.jpg" alt="Chania" height="300">
+    </div>
+
+    <div class="item">
+      <img src="images/slide/sd3.jpg" alt="Flower" height="300">
+    </div>
+
+    <div class="item">
+      <img src="images/slide/sd5.jpg" alt="Flower" height="300">
+    </div>
+  </div></div>
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<div class="col-lg-4 main"> 
+<h3 class="w3-blue w3-text-white w3-center w3-round-medium" style="padding:3px;margin:0px;width:100%;font-weight:bold">&nbsp;&nbsp;&nbsp;<span class="glyphicon glyphicon-time"></span>&nbsp;&nbsp;&nbsp;What's New &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<span><img src="images/start.png" style="margin-bottom:3px" onclick="function2()"></span>&nbsp;<span><img src="images/stop.png"style="margin-bottom:3px" onclick="function1()"></h3>
+<marquee direction="up" scrollamount="3"onmouseover="this.stop()" onmouseout="this.start()" id="myMarquee"><br><br><br>
+<li>
+<a href="docs/whatsnew/recruitment-for-the-post-of-C-and-D.pdf" target="_blank">Recruitment for the Post of C & D - Circular</a>&nbsp;&nbsp;<img src="images/new.gif">
+</li>
+<li>
+    <a href="docs/whatsnew/Important Circular to inmates and ex-inmates.PDF" target="_blank">
+         Important Circular to inmates and ex-inmates</a>
+</li>
+<li>
+    <a href="docs/whatsnew/call-letter_eligible-list.pdf" target="_blank"> C & D Recruitment - Interview call letter for eligible candidates</a>
+</li>
+<li>
+    <a href="docs/whatsnew/Inmates and Ex-inmates recruitment - Press release.pdf"target="_black">Inmates and Ex-inmates recruitment press  release</a>
+</li>
+<li>
+    <a href="docs/whatsnew/Foster Care advertisement.pdf"target="_blank">Advertisements - Foster Care</a>
+</li>
+<!--li>
+    <a href="docs/whatsnew/Circular - Comments and Suggestions for TN Rules 2016 for JJ Act 2015.pdf" target="_blank">Requesting for Comments and Suggestions on the TN draft rules 2016 for the JJ Act 2015</a>
+</li-->
+</marquee></div>
+</div>
+<div class="row" style="margin:0 20px 0 20px">
+<div class="col-md-8"><br>
+<p align="justify" class="maincontent" id="content" style="margin-top:0px">&nbsp; &nbsp; &nbsp; &nbsp; The State of Tamil Nadu stands first in providing care for the Children and Women in difficult circumstances and protection of their rights. The Madras Children Act, 1920 was enacted for the care and protection of children by which Tamil Nadu has become the first State in India to enact an exclusive legislation for children. The Department of Social Defence is committed to provide caring and joyful atmosphere with appropriate rehabilitation to the girls and women in distrees and moral danger. The Department of Social Defence is implementing three important legislations in the State, namely the Juvenile Justice (Care and Protection of Children) Act, 2000, the immoral Traffic (Prevention) Act, 1956 and Protection of children from Sexual Offences Act, 2012.</p>
+<p align="justify" class="maincontent" id="content">&nbsp; &nbsp; &nbsp; &nbsp; The Department of Social Defence is functioning under the Department of Social Welfare and Nutritious Meal Programme of the State Government. The Department is headed by the Director of Social Defence.</p> 
+</div>
+<div class="col-lg-4 col-md-4"><br>
+<a href="express-interest.html">
+<img src="images/express.gif"width="100" height="50"style="margin-left:35%" title="Click Me"></a><br><br>
+<h2 class="w3-center"><b>Visitor Counter</b></h2>
+<p class="w3-center"><?php require_once 'counter.php' ?></p><br>
+<div style="border:2px outset green;border-radius:10px">
+<p class="w3-center w3-text-teal">Send your Comments & Suggestions to</p>
+<p class="w3-center w3-text-indigo"><span class="glyphicon glyphicon-envelope">&nbsp;</span>dsdtngov@gmail.com</p></div>
+</div>
+</div>
+<div class="row w3-pale-yellow w3-round-medium"style="margin:0">
+<!--div class="col-lg-2 col-md-2"><h4 class="well w3-green w3-text-white w3-center w3-roundmedium"style="margin:0;font-weight:bold">Important Links</h4></div-->
+<div class="col-lg-12 col-md-12" style="padding:1px">
+<div style="width:100%; height:auto;">
+<marquee direction="left" scrollamount="5" onmouseover="this.stop()" onmouseout="this.start()" style="padding-top:0px;margin-left:40px" class="links">
+<a href="http://www.wcd.nic.in" target="_blank"><img src="images/links/wcd.jpg"></a>&nbsp;&nbsp;&nbsp;
+<a href="http://www.trackthemissingchild.gov.in" target="_blank"><img src="images/links/track-child.png"></a>&nbsp;&nbsp;&nbsp;
+<a href="http://www.tn.gov.in" target="_blank"><img src="images/links/tn.jpg"></a>&nbsp;&nbsp;&nbsp;
+<a href="http://www.childlineindia.org.in" target="_blank"><img src="images/links/child.jpg"></a>&nbsp;&nbsp;&nbsp;
+<a href="http://www.http://mhrd.gov.in/" target="_blank"><img src="images/links/mhrd.jpg"></a>&nbsp;&nbsp;&nbsp;
+<a href="http://www.stophumantrafficking-mha.nic.in/" target="_blank"><img src="images/links/home.jpg"></a>
+</marquee></div></div></div>
+<div class="contaier-fluid color"style="background-image:url(images/back.jpg);/*color:#566573;*/color:#fff">
+<!--div class="container-fluid" style="background-color:#2E4053;color:#fff"-->
+<div class="row"><br>
+<div class="col-sm-3">
+<p class="w3-center"><img src="images/links/service.png" width="100" height="100"></p>
+<h5 class="w3-center w3-text-teal"><b>Schemes & Services</b></h5>
+<ul style="margin-left:60px">
+<li><a href="ujjawala.html">Ujjawala Scheme</a></li>
+<li><a href="swadhar.html">Swadhar Scheme</a></li>
+<li><a href="Chid-line.html">Child Line</a></li>
+<li><a href="icps.html">ICPS</a></li>
+<li><a href="de-addiction-program.html">De-addiction Program(IRCA)</a></li>
+</ul>
+</div>
+<div class="col-md-3">
+<p class="w3-center"><img src="images/links/home.png" width="100" height="100"></p>
+<h5 class="w3-center w3-text-teal"><b>Government Homes</b></h5>
+<ul style="margin-left:60px">
+<li><a href="govt-homes.html">Child Homes</a></li>
+<li><a href="govt-obshomes.html">Observation Homes</a></li>
+<li><a href="govt-aftercare.html">AfterCare Institutions</a></li>
+<li><a href="govt-special.html">Special Homes</a></li>
+<li><a href="#">Reception Units</a></li>
+</ul></div>
+<div class="col-md-3">
+<p class="w3-center"><img src="images/links/documents.png" width="100" height="100"></p>
+<h5 class="w3-center w3-text-teal"><b>Documents</b></h5>
+<ul style="margin-left:60px">
+<li><a href="acts-rules.html">Acts and Rules</a></li>
+<li><a href="govt-orders.html">Government Orders</a></li>
+<li><a href="policies.html">Policies</a></li>
+<li><a href="download.html">Downloads</a></li>
+<li><a href="notifications.html">Notifications</a></li>
+</ul>
+</div>
+<div class="col-md-3">
+<p class="w3-center"><img src="images/links/home.png" width="100" height="100"></p>
+<h5 class="w3-center w3-text-teal"><b>NGO Homes</b></h5>
+<ul style="margin-left:60px">
+<li><a href="non-childhomes.html">Child Homes</a></li>
+<li><a href="non-obshomes.html">Observation Homes</a></li>
+<li><a href="resource-centers.html">Resource Centers</a></li>
+<li><a href="reception-unit.html">Reception Units</a></li>
+<li><a href="de-addiction.html">De-Addiction Centers</a></li>
+</ul>
+</ul></div>
+</div><br><br></div>
+<div class="container-fluid footer" style="background-image:url(images/foot.png);color:white;">
+<div class="row">
+<div class="col-md-4 col-sm-4"><br>
+<p>Contents Owned, Maintained and Updated By</span></p><span style="font-size:10px">
+<p>Department of Social Defence</p>
+<p>No.300 Purasawalkam High Road</p>
+<p>Kellys, Chennai-600010</p>
+ <p>044-2642 6421, 2642 7022</p>
+ <p><span class="glyphicon glyphicon-envelope" width="30" height="30"></span><!--Email: -->&nbsp;&nbsp;dsd@tn.nic.in</span></p>
+</div>
+<div class="col-md-4 col-sm-4"><br>
+<p class="w3-center"> Site Last Updated&nbsp;:&nbsp;16-11-2016&nbsp;&nbsp;10:56 AM</p--><br>
+<p style="font-size:12px;"class="w3-center"><a href="feedback.html">Feedback</a>&nbsp;/&nbsp;<a href="disclaimer.html">Disclaimer</a>&nbsp;/&nbsp;<a href="contact-dept.html">Contact Us</a>&nbsp;/&nbsp;<a href="sitemap.html">Site Map</a></p>
+<p class="w3-center w3-text-aqua" style="font-weight:bold"><font size="1">Site best viewed at 1024 x 768 resolution</font></p>
+</div>
+<div class="col-md-4 col-sm-4"><br>
+<p> Designed & Developed By</p><span style="font-size:10px">
+<p>Directorate of e-Governance / Tamil Nadu e-Governance Agency</p>
+<p>No. 5/9 Kavignar Bharathidasan Road,</p>
+<p>Alwarpet, Chennai-600018</p>
+<p><a href="http://tnega.in" target="_blank"><span class="glyphicon glyphicon-globe" width="30" height="30"></span>&nbsp;&nbsp;tnega.in</a></span></p>
+</div></div>
+</div></div>
+</div>
+</body>
+</html>
---
+++ quick-links.php
@@ -0,0 +1,61 @@
+<style>
+.color li{color:indigo}
+</style>
+<div class="contaier-fluid color"style="background-image:url(images/back.jpg);/*color:#566573;*/color:#fff">
+<!--div class="container-fluid" style="background-color:#2E4053;color:#fff"-->
+<div class="row"><br>
+<div class="col-sm-3">
+<p class="w3-center"><img src="images/links/service.png" width="100" height="100"></p>
+<h5 class="w3-center w3-text-teal"><b>Schemes & Services</b></h5>
+<ul style="margin-left:60px">
+<li><a href="ujjawala.html">Ujjawala Scheme</a></li>
+<li><a href="swadhar.html">Swadhar Scheme</a></li>
+<li><a href="Chid-line.html">Child Line</a></li>
+<li><a href="icps.html">ICPS</a></li>
+<li><a href="central-sectors.html">Central Sector</a></li>
+</ul>
+</div>
+<div class="col-md-3">
+<p class="w3-center"><img src="images/links/home.png" width="100" height="100"></p>
+<h5 class="w3-center w3-text-teal"><b>Government Homes</b></h5>
+<ul style="margin-left:60px">
+<li><a href="govt-homes.html">Child Homes</a></li>
+<li><a href="govt-obshomes.html">Observation Homes</a></li>
+<li><a href="govt-aftercare.html">AfterCare Institutions</a></li>
+<li><a href="govt-special.html">Special Homes</a></li>
+<li><a href="#">Reception Units</a></li>
+</ul></div>
+<div class="col-md-3">
+<p class="w3-center"><img src="images/links/documents.png" width="100" height="100"></p>
+<h5 class="w3-center w3-text-teal"><b>Documents</b></h5>
+<ul style="margin-left:60px">
+<li><a href="acts-rules.html">Acts and Rules</a></li>
+<li><a href="govt-orders.html">Government Orders</a></li>
+<li><a href="policies.html">Policies</a></li>
+<li><a href="download.html">Downloads</a></li>
+<li><a href="notifications.html">Notifications</a></li>
+</ul>
+</div>
+<div class="col-md-3">
+<p class="w3-center"><img src="images/links/home.png" width="100" height="100"></p>
+<h5 class="w3-center w3-text-teal"><b>NGO Homes</b></h5>
+<ul style="margin-left:60px">
+<li><a href="non-childhomes.html">Child Homes</a></li>
+<li><a href="non-obshomes.html">Observation Homes</a></li>
+<li><a href="resource-centers.html">Resource Centers</a></li>
+<li><a href="reception-unit.html">Reception Units</a></li>
+<li><a href="de-addiction.html">De-Addiction Centers</a></li>
+</ul>
+</ul></div>
+<!--div class="col-md-2">
+<p class="w3-center"><img src="images/links/programs.png" width="100" height="100"></p>
+<h5 class="w3-center w3-text-aqua"><b>Programs</b></h5>
+<ul>
+<li><a href="child-development.html">Child Development</a></li>
+<li><a href="women-development.html">Women Development</a></li>
+<li><a href="trafficking.html">Trafficking</a></li>
+<li><a href="socialdefence.html">Social Defence</a></li>
+<li><a href="other-programs.html">Other Programs</a></li>
+</ul>
+</div--></div><br><br></div>
+
