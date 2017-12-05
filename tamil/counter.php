69b8efda043be2d44451b0419f8874420be1a8c5
deena_projects <deenadayalan17@gmail.com>
2017-12-05 12:20:58 +0530

    Add files via upload

---
+++ tamil/counter.php
@@ -0,0 +1,95 @@
+<?php
+#session_start();
+$counter_name = "counter.txt";
+
+// Check if a text file exists. If not create one and initialize it to zero.
+if (!file_exists($counter_name)) {
+  $f = fopen($counter_name, "w");
+  fwrite($f,"0");
+  fclose($f);
+}
+
+// Read the current value of our counter file
+$f = fopen($counter_name,"r");
+$counter = ( int ) fread($f, filesize($counter_name));
+fclose($f);
+
+// Has visitor been counted in this session?
+// If not, increase counter value by one
+if(!isset($_SESSION['hasVisited'])){
+  $_SESSION['hasVisited']="yes";
+  $counter++;
+  $f = fopen($counter_name, "w");
+  fwrite($f, $counter);
+  fclose($f); 
+}
+//echo "Visitor Counter:<b> $counterVal</b>";
+$arr=array();
+	$num=$counter;
+	if($counter<10)
+			$con=0;
+		else if($counter<100)
+			$con=1;
+		else if($counter<1000)
+			$con=2;
+			else if($counter<10000)
+				$con=3;
+				else if($counter<100000)
+					$con=4;
+					else if($counter<1000000)
+						$con=5;
+						else if($counter<10000000)
+							$con=6;
+							else if($counter<100000000)
+								$con=7;
+								else if($counter<1000000000)
+									$con=8;
+									else if($counter<10000000000)
+									$con=9;
+							
+					
+						
+	while($num>0){
+		//or($i=0;$i<count($arr);$i++)
+		//{
+		$arr[]=$num%10;
+		$num = $num/10;
+	}
+	for($i=$con;$i>=0;$i--)
+	{
+	switch($arr[$i])
+{ 
+case 0:
+echo "<img src=images/number/0.png width=30 height=30>";
+break;
+case 1:
+echo "<img src=images/number/1.png width=30 height=30>";
+break;
+case 2:
+echo "<img src=images/number/2.png width=30 height=30>";
+break;
+case 3:
+echo "<img src=images/number/3.png width=30 height=30>";
+break;
+case 4:
+echo "<img src=images/number/4.png width=30 height=30>";
+break;
+case 5:
+echo "<img src=images/number/5.png width=30 height=30>";
+break;
+case 6:
+echo "<img src=images/number/6.png width=30 height=30>";
+break;
+case 7:
+echo "<img src=images/number/7.png width=30 height=30>";
+break;
+case 8:
+echo "<img src=images/number/8.png width=30 height=30>";
+break;
+case 9:
+echo "<img src=images/number/9.png width=30 height=30>";
+break;
+}
+}
+//echo $count;
+?>
---
+++ tamil/index.php
@@ -0,0 +1,281 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<?php session_start(); ?>
+<head>
+<title>முகப்பு - சமூக பாதுகாப்புத்துறை</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.2/jquery.min.js"></script>
+<script type="javascript" src="style/js/bootstrap.min.js"></script>
+<script src="style/js/jquery-1.12.3.min.js"></script>
+<script src="style/js/font.js"></script>
+<link rel="stylesheet" type="text/css" href="../style/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="../style/css/w3.css">
+<link rel="stylesheet" type="text/css" href="menu.css">
+<link rel="stylesheet" type="text/css" href="social-defence.css">
+<link rel="stylesheet" type="text/css" href="../style/css/font-change.css">
+<style>
+marquee li{list-style-type:none;margin:10px 0 0 5px;border-bottom:1px solid #48C9B0;border-left:15px solid #48C9B0;background-color:#A2D9CE;}marquee li:hover{border-left:15px solid #48C9B0;background-color:#A2D9CE;padding-left:3px} marquee li,a:hover{text-decoration:none}
+.links img{border:2px solid maroon !important;border-radius:5px;margin-bottom:0px;margin-top:2px}
+.color li{color:indigo}
+</style>
+</head>
+<body>
+<div class="wrapper">
+<div style="background-image:url(../images/background.png">
+<div class="row">
+<div class="col-lg-8 col-md-8 col-sm-8">
+<img src="../images/logo/logo-tamil.png"/></div>
+<div class="col-lg-4 col-md-4 col-sm-4">
+<div id="fontlinks">
+		<a href="#" id="incfont" class="button buttonfont">A+</a>
+		<a href="#" id="decfont" class="button buttonfont">A-</a>
+                <a href="#" id="black" class="buttonblack buttonfont">A</a>
+                <a href="#" id="white" class="buttonwhite buttonfont">A</a>
+             		</div>
+</div></div>
+<div class="menu">
+<ul class="main-navigation">
+  <li><a class="w3-hover-blue" href="index.php"><img src="../images/home.png" width="35" height="35" style="margin-top:-8px"></a></li>
+  <li><a class="w3-hover-blue" href="#">அமைப்பு</a>
+  <ul>
+      <li><a href="org-struct.html">நிறுவன கட்டமைப்பு</a></li>
+      <li><a href="history.html">துறையின் வரலாறு</a></li>
+      <li><a href="admin-setup.html">நிர்வாக அமைப்பு</a></li>
+  </ul>
+  </li>
+  <li><a class="w3-hover-blue" href="citizen-charters.html">மக்கள் சாசனம்</a></li>
+  <li><a class="w3-hover-blue" href="#">திட்டங்கள் & சேவைகள்
+        <span class="caret"></span></a>
+     <ul>
+        <li><a href="ujjawala.html">உஜ்ஜவாலா திட்டம்</a></li>
+          <li><a href="swadhar.html">சுவதார் திட்டம்</a></li>
+	  <li><a href="child-line.html">சைல்டு லைன்</a></li>
+          <li><a href="icps.html">ஒருங்கினைந்த குழந்தைகள் பாதுகாப்புத் திட்டம்</a></li>
+	  <li><a href="central-sectors.html">மத்திய துறையின் மது மற்றும் போதை பொருள் பயன்படுத்துதல் தடுப்பு உதவி திட்டம் </a></li>
+        </ul>
+      </li>
+   <li><a class="w3-hover-blue" href="#">ஆவணங்கள்
+        <span class="caret"></span></a>
+        <ul>
+        <li> <a href="acts-rules.html">சட்டங்கள் மற்றும் விதிகள்</a></li>
+          <li><a href="govt-orders.html">அரசாணைகள்</a></li>
+          <li><a href="policies.html">கொள்கைகள்</a></li>
+          <li><a href="download.html">பதிவிறக்கங்கள்</a></li>
+	  <li><a href="notifications.html">அறிவிப்புகள்</a></li>
+        </ul>
+      </li>
+       <li>
+        <a class="w3-hover-blue" href="#">நிறுவனங்கள்
+        <span class="caret"></span></a>
+        <ul>
+         <li>  <a href="juvenile-justice.html">இளைஞர் நீதிக் குழுமம்</a></li>
+          <li> <a href="child-welfare.html">குழந்தைகள் நலக் குழு</a></li>
+         <li> <a href="#">அரசு இல்லங்கள்<span class="caret"></span></a>
+           <ul>
+           <li><a href="govt-homes.html">குழந்தைகள் இல்லம்</a></li>
+           <li><a href="govt-obshomes.html">கூர்நோக்கு இல்லம்</a></li>
+           <li><a href="govt-aftercare.html">பிற்காப்பு நிறுவனம்</a> </li>
+           <li><a href="govt-special.html">சிறப்பு இல்லம்</a></li>
+           <li><a href="#">வரவேற்புப் பிரிவு</a></li></ul>
+           <li><a href="#">அரசு சாரா இல்லம்<span class="caret"></span></a>
+           <ul>
+           <li><a  href="non-childhomes.html">குழந்தைகள் இல்லம்</a></li>
+          <li> <a  href="non-obshomes.html">கூர்நோக்கு இல்லம்</a></li>
+          <li> <a  href="resource-centers.html">வள மையம்</a></li>
+          <li> <a  href="under.html">வரவேற்புப் பிரிவு</a></li>
+	   <li><a  href="de-addiction.html">குடி மற்றும் போதை பொருள் மறுவாழ்வு மையம்</a></li>
+           <li><a  href="ujjawala.html">உஜ்ஜவாலா</a></li>
+           <li><a  href="swadhar.html">சுவதார்</a></li></ul>
+           <li><a href="#">குழந்தைகள் இல்லங்களின் பதிவின் விவரம் <span class="caret"></span></a>
+          <ul>
+          <li><a  href="registered.html">பதிவு பெற்ற இல்லங்கள்</a></li>
+          <li><a  href="applied-renewal.html">பதிவினை புதுபிக்க கருத்துரு சமர்பித்த இல்லங்கள்</a></li>
+         <li> <a  href="notapplied-renewal.html">பதிவினை புதுபிக்க கருத்துரு சமர்பிக்காத இல்லங்கள்</a> </li>
+	  <li><a  href="applied-regis.html">பதிவிற்கு கருத்துரு சமர்பித்த பதிவுபெறாத இல்லங்கள்</a></li>
+          <li><a  href="notapplied-regis.html">பதிவிற்கு கருத்துரு சமர்பிக்காத பதிவுபெறாத இல்லங்கள்</a></li>
+        </ul></ul>
+      </li>
+  <li><a class="w3-hover-blue" href="#">தகவல் பெறும் உரிமை
+        <span class="caret"></span></a>
+        <ul>
+         <li> <a href="docs/acts/RTI.pdf"target="_blank">தகவல் பெறும் உரிமை சட்டம்</a></li>
+         <li> <a href="http://www.tnsic.gov.in/"target="_blank">தமிழ்நாடு தகவல் ஆணையம் </a></li>
+        </ul>
+      </li>
+  <li><a class="w3-hover-blue" href="#">பதிப்புகள்
+        <span class="caret"></span></a>
+        <ul>
+         <li><a href="annual-reports.html">ஆண்டறிக்கை</a></li>
+         <li><a href="docs/policynote-2015-16/Policy Note 2015-16-DSD- English .pdf"target="_blank">கொள்கை விளக்கக் குறிப்பு</a></li>
+         <li><a href="iec.html">தகவல் கல்வி தொடர்பு</a></li>
+        </ul>
+      </li>
+ <li><a class="w3-hover-blue" href="#">தொடர்புக்கு
+        <span class="caret"></span></a>
+        <ul>
+        <li><a href="contact-dept.html">சமூக பாதுகாப்புத் துறை இயக்குநரகம்</a></li>
+        <li><a href="contact-reg.html">மண்டல அலுவலகம்</a></li>
+        <li><a href="contact-scps.html">மாநில குழந்தைகள் பாதுகாப்பு சங்கம்</a></li> 
+        <li><a href="docs/Contact/DCPU.pdf" target="_blank">மாவட்ட குழந்தைகள் பாதுகாப்பு அலகு</a></li>
+                <li><a href="contacts.html">மாநில சட்ட சேவை ஆணையம்</a></ul></li>
+<li><a class="w3-hover-blue" href="gallery.html">புகைப்படங்கள்</a></li>
+</ul>
+</div></div>
+<marquee direction="left" scrollamount="5"onmouseover="this.stop()" onmouseout="this.start()" class="w3-text-purple"><strong>குழந்தைகளுக்கான இலவச தொலைபேசி உதவி எண் : 1098 &nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;பெண்களுக்கான இலவச தொலைபேசி உதவி எண் :1091&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;To View this Site in English &nbsp;&nbsp;</strong><font color="blue"><a href="../index.php">Click Here</a></font--></marquee>
+<br>
+<div class="row">
+<div class="col-md-7 main" style="margin-left:30px">
+<div id="myCarousel" class="carousel slide box" data-ride="carousel">
+  <!-- Indicators -->
+  <ol class="carousel-indicators">
+    <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+    <li data-target="#myCarousel" data-slide-to="1"></li>
+    <li data-target="#myCarousel" data-slide-to="2"></li>
+    <li data-target="#myCarousel" data-slide-to="3"></li>
+  </ol>
+
+  <!-- Wrapper for slides -->
+  <div class="carousel-inner" role="listbox">
+    <div class="item active">
+      <img src="../images/slide/sd1.jpg" alt="Chania" height="300">
+    </div>
+
+    <div class="item">
+      <img src="../images/slide/sd2.jpg" alt="Chania" height="300">
+    </div>
+
+    <div class="item">
+      <img src="../images/slide/sd3.jpg" alt="Flower" height="300">
+    </div>
+
+    <div class="item">
+      <img src="../images/slide/sd5.jpg" alt="Flower" height="300">
+    </div>
+  </div></div>
+  <!-- Left and right controls -->
+  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div>
+<div class="col-lg-4 main"> 
+<h3 class="w3-blue w3-text-white w3-center w3-round-medium" style="padding:3px;margin:0px;width:100%;font-weight:bold"><span class="glyphicon glyphicon-time"></span>&nbsp;&nbsp;&nbsp;புதிய அறிவிப்புகள்</h3>
+<marquee direction="up" scrollamount="3"onmouseover="this.stop()" onmouseout="this.start()">
+<li><a href="docs/whatsnew/Inmates and Ex-inmates recruitment - Press release.pdf"target="_black">Inmates and Ex-inmates recruitment press release</a></li>
+<li><a href="docs/whatsnew/call-letter_eligible-list.pdf" target="_blank"> C & D Recruitment - Interview call letter for eligible candidates</a><img src="../images/new.gif"></li>
+<li><a href="docs/whatsnew/Foster Care advertisement.pdf"target="_blank">Advertisements - Foster Care</a></li>
+<li></li>
+<li><a href="docs/whatsnew/Circular - Comments and Suggestions for TN Rules 2016 for JJ Act 2015.pdf" target="_blank">Requesting for Comments and Suggestions on the TN draft rules 2016 for the JJ Act 2015<img src="../images/new.gif"></a></li>
+</marquee><br><br><hr>
+<p class="w3-right" style="margin:-15px 10px 0 0"><a href="whatsnew.html"><font color="maroon"><b>View All</b></font></a></p></div>
+</div>
+<div class="row" style="margin:0 20px 0 20px">
+<div class="col-md-8" id="content"><br>
+<p align="justify" class="maincontent"style="margin-top:0px">&nbsp; &nbsp; &nbsp; &nbsp;துன்புறு சூழ்நிலைகளில் இருக்கும் குழந்தைகள் மற்றும் பெண்களுடைய உரிமைகளை உறுதிசெய்வதிலும், அவர்களுக்கு பாதுகாப்பு மற்றும் பராமரிப்பு வழங்குவதில் தமிழ்நாடு முதல் நிலை வகித்துவருகிறது. குழந்தை பராமரிப்பு மற்றும் பாதுகாப்பினை உறுதி செய்யும் வகையில் “மெட்ராஸ் குழந்தைகள் சட்டம்” என்னும் பிரத்தியேக சட்டத்தினை 1920-ம் ஆண்டில் இந்தியாவிலேயே முதலில் மெட்ராஸ் ராஜதானியில் இயற்றப்பட்டது. துன்பம் மற்றும் தார்மீக ஆபத்துகளில் இருக்கும் சிறுமியர் மற்றும் பெண்களுக்கு ஏதுவான மறுவாழ்வு சேவைகள், அதனுடன் மகிழ்ச்சி நிறைந்த மற்றும் அக்கறை மிகுந்த சூழலை வழங்கும் முனைப்புடன் சமூக பாதுகாப்புத் துறையானது செயல்படுகிறது. சமூக பாதுகாப்புத் துறையானது, இளஞ்சிறார் நீதிச் (குழந்தைகள் பாதுகாப்பு மற்றும் பராமரிப்பு) சட்டம் 2015, ஒழுக்க நெறி பிறழ்தல் (தடுப்பு) சட்டம் 1956 ஆகியவற்றை செயல்படுத்துதல் மற்றும் பாலியல் குற்றத்திலிருந்து குழந்தையை பாதுகாக்கும் சட்டம் 2012-ன் கீழ் வரக்கூடிய பாதிப்புக் குள்ளானவர்களுக்கு உரிய மறுவாழ்வு நடவடிக்கை எடுத்தல் ஆகியப்பணிகளை ஆற்றி வருகின்றது.</p>
+<p align="justify" class="maincontent">&nbsp; &nbsp; &nbsp; &nbsp;  தமிழக அரசின் சமூக நலம் மற்றும் சத்துணவுத் திட்டத் துறையின் கீழ் சமூக பாதுகாப்புத் துறையானது இயக்குநர், சமூக பாதுகாப்புத் துறை அவர்களின் தலைமையில் இயங்கிவருகின்றது.</p> 
+</div>
+<div class="col-lg-4 col-md-4"><br>
+<a href="express-interest.php">
+<img src="../images/express.gif"width="100" height="50"style="margin-left:35%" title="Click Me"></a><br><br>
+<h4 class="w3-center"><b>பார்வையாளர்கள் எண்ணிக்கை</b></h4>
+<p class="w3-center"><?php require_once './counter.php' ?></p><br>
+<div style="border:2px outset green;border-radius:10px">
+<p class="w3-center w3-text-teal">உங்கள் கருத்துக்கள் மற்றும் ஆலோசனைகளை கீழே உள்ள மின் அஞ்சலுக்கு அனுப்பவும்</p>
+<p class="w3-center w3-text-indigo"><span class="glyphicon glyphicon-envelope">&nbsp;</span>dsdtngov@gmail.com</p></div>
+</div>
+</div>
+<div class="row w3-pale-yellow w3-round-medium"style="margin:0">
+<!--div class="col-lg-2 col-md-2"><h4 class="well w3-green w3-text-white w3-center w3-roundmedium"style="margin:0;font-weight:bold">Important Links</h4></div-->
+<div class="col-lg-12 col-md-12" style="padding:1px">
+<div style="width:100%; height:auto;">
+<marquee direction="left" scrollamount="5" onmouseover="this.stop()" onmouseout="this.start()" style="padding-top:0px;margin-left:40px" class="links">
+<a href="http://www.wcd.nic.in" target="_blank"><img src="../images/links/wcd.jpg"></a>&nbsp;&nbsp;&nbsp;
+<a href="http://www.trackthemissingchild.gov.in" target="_blank"><img src="../images/links/track-child.png"></a>&nbsp;&nbsp;&nbsp;
+<a href="http://www.tn.gov.in" target="_blank"><img src="../images/links/tn.jpg"></a>&nbsp;&nbsp;&nbsp;
+<a href="http://www.childlineindia.org.in" target="_blank"><img src="../images/links/child.jpg"></a>&nbsp;&nbsp;&nbsp;
+<a href="http://www.http://mhrd.gov.in/" target="_blank"><img src="../images/links/mhrd.jpg"></a>&nbsp;&nbsp;&nbsp;
+<a href="http://www.stophumantrafficking-mha.nic.in/" target="_blank"><img src="../images/links/home.jpg"></a>
+</marquee></div></div></div>
+<div class="contaier-fluid color"style="background-image:url(images/back.jpg);/*color:#566573;*/color:#fff">
+<!--div class="container-fluid" style="background-color:#2E4053;color:#fff"-->
+<div class="row"><br>
+<div class="col-sm-3">
+<p class="w3-center"><img src="../images/links/service.png" width="100" height="100"></p>
+<h5 class="w3-center w3-text-teal"><b>திட்டங்கள் மற்றும் சேவைகள்</b></h5>
+<ul style="margin-left:60px">
+<li><a href="ujjawala.html">உஜ்ஜவாலா திட்டம்</a></li>
+<li><a href="swadhar.html">சுவதார் திட்டம்</a></li>
+<li><a href="Chid-line.html">சைல்டு லைன்</a></li>
+<li><a href="icps.html">ஒருங்கினைந்த குழந்தைகள் பாதுகாப்புத் திட்டம்</a></li>
+<li><a href="central-sectors.html">மத்திய துறையின் மது மற்றும் போதை பொருள் பயன்படுத்துதல் தடுப்பு உதவி திட்டம் </a></li>
+</ul>
+</div>
+<div class="col-md-3">
+<p class="w3-center"><img src="../images/links/home.png" width="100" height="100"></p>
+<h5 class="w3-center w3-text-teal"><b>அரசு இல்லங்கள்</b></h5>
+<ul style="margin-left:60px">
+<li><a href="govt-homes.html">குழந்தைகள் இல்லம்</a></li>
+<li><a href="govt-obshomes.html">கூர்நோக்கு இல்லம்</a></li>
+<li><a href="govt-aftercare.html">பிற்காப்பு நிறுவனம்</a></li>
+<li><a href="govt-special.html">சிறப்பு இல்லம்</a></li>
+<li><a href="reception-unit.html">வரவேற்புப் பிரிவு</a></li>
+</ul></div>
+<div class="col-md-3">
+<p class="w3-center"><img src="../images/links/documents.png" width="100" height="100"></p>
+<h5 class="w3-center w3-text-teal"><b>ஆவணங்கள்</b></h5>
+<ul style="margin-left:60px">
+<li><a href="acts-rules.html">சட்டங்கள் மற்றும் விதிகள்</a></li>
+<li><a href="govt-orders.html">அரசாணைகள்</a></li>
+<li><a href="policies.html">கொள்கைகள்</a></li>
+<li><a href="download.html">பதிவிறக்கங்கள்</a></li>
+<li><a href="notifications.html">அறிவிப்புகள்</a></li>
+</ul>
+</div>
+<div class="col-md-3">
+<p class="w3-center"><img src="../images/links/home.png" width="100" height="100"></p>
+<h5 class="w3-center w3-text-teal"><b>அரசு சாரா இல்லங்கள்</b></h5>
+<ul style="margin-left:60px">
+<li><a href="non-childhomes.html">குழந்தைகள் இல்லம்</a></li>
+<li><a href="non-obshomes.html">கூர்நோக்கு இல்லம்</a></li>
+<li><a href="resource-centers.html">வள மையம்</a></li>
+<li><a href="reception-unit.html">வரவேற்புப் பிரிவு</a></li>
+<li><a href="de-addiction.html">குடி மற்றும் போதை பொருள் மறுவாழ்வு மையம்</a></li>
+</ul>
+</ul></div>
+</div><br><br></div>
+<div class="container-fluid footer" style="background-image:url(../images/foot.jpg);color:white;">
+<div class="row">
+<div class="col-md-4 col-sm-4"><br>
+<p>உள்ளடக்கங்கள், பராமரிப்பு மற்றும் பதிவேற்றம்</span></p><span style="font-size:10px">
+<p>சமூக பாதுகாப்புத் துறை</p>
+<p>எண்: 300, புரசைவாக்கம் நெடுஞ் சாலை</p>
+<p>கெல்லீஸ், சென்னை – 600010</p>
+ <p>044-2642 6421, 2642 7022</p>
+ <p><span class="glyphicon glyphicon-envelope" width="30" height="30"></span><!--Email: -->&nbsp;&nbsp;dsd@tn.nic.in</span></p>
+</div>
+<div class="col-md-4 col-sm-4"><br>
+<p class="w3-center"> கடைசியாக தளத்தில் பதிவேற்றம் <br><br><br>செய்யப்பட்ட நாள்:&nbsp;&nbsp;15-09-2016&nbsp;&nbsp;04:30 PM</p><br>
+<p style="font-size:12px;"class="w3-center"><a href="feedback.html">கருத்து</a>&nbsp;/&nbsp;<a href="disclaimer.html">பொறுப்புத்துறப்பு</a>&nbsp;/&nbsp;<a href="contact-dept.html">தொடர்புக்கு</a>&nbsp;/&nbsp;<a href="sitemap.html">தள வரைபடம்</a></p>
+<p class="w3-center w3-text-aqua" style="font-weight:bold"><font size="1">Site best viewed at 1024 x 768 resolution</font></p>
+</div>
+<div class="col-md-4 col-sm-4"><br>
+<p> வடிவமைப்பு மற்றும் உருவாக்கம்</p><span style="font-size:10px">
+<p>மின்-ஆளுமை இயக்குனரகம் / தமிழ் நாடு மின்-ஆளுமை  முகமை </p>
+<p>No. 5/9 கவிஞர் பாரதிதாசன் சாலை,</p>
+<p>ஆழ்வார்பேட்டை , சென்னை -600018</p>
+<p><a href="http://tnega.in" target="_blank"><span class="glyphicon glyphicon-globe" width="30" height="30"></span>&nbsp;&nbsp;tnega.in</a></span></p>
+</div></div>
+</div>
+</div>
+</body>
+</html>
